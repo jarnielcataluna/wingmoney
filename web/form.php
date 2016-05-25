07d5719e30cabaf1638e18dbe63b50e865854545
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-05-25 18:50:36 +0800

    Added Firebase Setup Added Firebase Form Integration Added Ajax Form Integration Added Simple Validations Added Facebook Oauth Integration

---
+++ web/form.php
@@ -0,0 +1,102 @@
+<form class="wing-form" method="post" action="exec/validate.php" novalidate>
+    <input type="hidden" name="fb_uid" id="fb_uid"/>
+    <input type="hidden" name="fb_fname" id="fb_fname"/>
+    <input type="hidden" name="fb_lname" id="fb_lname"/>
+    <input type="hidden" name="fb_email" id="fb_email"/>
+    <input type="hidden" name="fb_profile_pic" id="fb_profile_pic"/>
+
+    <p class="form-text">Or fill up this form</p>
+    <div class="form-row">
+        <div class="input-wrap">
+            <input type="text" id="last-name" name="lname" value="" required="required">
+            <label for="last-name">Last Name</label>
+            <div class="required-tooltip right">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="input-wrap">
+            <input type="text" id="first-name" name="fname" value="" required="required">
+            <label for="first-name">First Name</label>
+            <div class="required-tooltip right">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="input-wrap input-half">
+            <input type="text" id="birthday" name="dob" value="" required="required">
+            <label for="birthday">Birthday</label>
+            <div class="required-tooltip left">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="input-wrap input-half gender">
+            <select class="custom-select" name="gender" id="gender">
+                <option value="0">Gender</option>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+            </select>
+            <div class="required-tooltip right">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="clr"></div>
+        <div class="input-wrap">
+            <input type="text" id="contact-num" name="contact" value="" required="required">
+            <label for="contact-num">Contact Number</label>
+            <div class="required-tooltip left">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="input-wrap">
+            <input type="email" id="account-email" name="email" value="" required="required" onkeyup="this.setAttribute('value', this.value);">
+            <label for="account-email">Email</label>
+            <div class="required-tooltip right">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="input-wrap input-half id-type">
+            <select class="custom-select" name="id_type">
+                <option value="0">ID type</option>
+                <option value="1">Sample 1</option>
+                <option value="2">Sample 2</option>
+                <option value="3">Sample 3</option>
+            </select>
+            <div class="required-tooltip left">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="input-wrap input-half">
+            <input type="text" id="id-number" name="id_number" value="" required="required">
+            <label for="id-number">ID Number</label>
+            <div class="required-tooltip right">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <div class="clr"></div>
+        <div class="input-wrap choose-currency">
+            <label>Choose Currency:</label>
+            <div class="custom-radio">
+                <input type="radio" id="usd" name="currency" checked>
+                <label for="usd"><span></span>USD</label>
+            </div>
+            <div class="custom-radio">
+                <input type="radio" id="Khmer" name="currency">
+                <label for="Khmer"><span></span>Khmer</label>
+            </div>
+        </div>
+        <div class="input-wrap select-branch">
+            <select class="custom-select" name="store_branch">
+                <option value="0">Select Store/Branch</option>
+                <option value="1">Sample</option>
+                <option value="2">Sampleasdas asd asdsad</option>
+                <option value="3">Sample adasd</option>
+            </select>
+            <div class="required-tooltip left">
+                <p>This field is required <span>*</span></p>
+            </div>
+        </div>
+        <hr>
+        <div class="input-wrap text-center btn-wrapper">
+            <input type="submit" class="btn btn-orange" name="" value="SUBMIT" />
+        </div>
+    </div>
+</form>
--- web/js/script.js
+++ web/js/script.js
@@ -1,13 +1,115 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCGlkjuXIbRKSb5GHsi8JUZP6YGAxTUAsI",
-    authDomain: "propelrr-wing-money.firebaseapp.com",
-    databaseURL: "https://propelrr-wing-money.firebaseio.com",
+    apiKey: "AIzaSyDx2elBwIOx7X_3YErUUHF9pgXmhXrGSxI",
+    authDomain: "wing-money-propelrr.firebaseapp.com",
+    databaseURL: "https://wing-money-propelrr.firebaseio.com",
     storageBucket: ""
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+function writeUserData(lname, fname, email, contact, gender, id_type, id_number, bday, branch, currency, fb_details) {
+    database.ref('leads').push({
+        lastname: lname,
+        firstname: fname,
+        email: email,
+        bday: bday,
+        contact: contact,
+        gender: gender,
+        id_type: id_type,
+        id_number: id_number,
+        currency: currency,
+        branch: branch,
+        fb_details: fb_details
+    });
+}
+
 $(document).ready(function(){
+
+    // Initialize Facebook
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '501745020012175',
+            xfbml      : true,
+            version    : 'v2.3'
+        });
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    $('#useFacebook').click(function(e) {
+        e.preventDefault();
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                useFacebook();
+            } else {
+                FB.login(function(){
+                    useFacebook();
+                }, {scope: 'publish_actions, email, public_profile, user_birthday, user_location, user_likes, user_education_history, pages_messaging_phone_number'});
+            }
+        });
+    });
+
+    function useFacebook() {
+        FB.api('/me', {fields: 'id,first_name,last_name,birthday,gender,about,email,education,bio,devices,hometown,location,likes,work,public_key'}, function(fbResponse) {
+
+
+            console.log(fbResponse);
+            var _fbUid = fbResponse.id;
+            var _fname = fbResponse.first_name;
+            var _lname = fbResponse.last_name;
+            var _birthday = fbResponse.birthday;
+            var _email = fbResponse.email;
+            var _gender = fbResponse.gender;
+
+            $('#fb_uid').val(_fbUid);
+            $('#fb_fname').val(_fname);
+            $('#fb_lname').val(_lname);
+            $('#fb_email').val(_email);
+
+            $('#last-name').val(_lname);
+            $('#first-name').val(_fname);
+            $('#birthday').val(_birthday);
+            $('#account-email').val(_email);
+            if(_gender === 'male'){
+                $('#gender').val('M');
+                $('#gender').siblings('.custom-select-display').html('Male');
+            }else if(_gender === 'female'){
+                $('#gender').val('F');
+                $('#gender').siblings('.custom-select-display').html('Female');
+            }
+            $('#email').val(_email);
+
+
+        });
+
+        FB.api('/me/picture?type=large', function(fbResponse) {
+            $('#fb_profile_pic').val(fbResponse.data.url);
+        });
+
+
+    }
+
+
+
+        $('input').on('change paste keyup',function(){
+            console.log('working');
+            if($(this).val().length > 0){
+                $(this).closest('.input-wrap').removeClass('error');
+            }
+        });
+
+
+
+
     $('.wing-form').submit(function(e){
         $('.input-wrap').addClass('error');
         isvalidate = false;
@@ -77,10 +179,44 @@ $(document).ready(function(){
             isvalidate = false;
         }
 
-        console.log(isvalidate);
-
         if( isvalidate == true) {
-            return true;
+            $('.wing-form-wrap').addClass('overlay');
+            var that = $(this),
+                url = that.attr('action'),
+                type = that.attr('method'),
+                page = that.attr('page'),
+                data = {};
+
+            that.find('[name]').each(function (index, value) {
+                var that = $(this),
+                    name = that.attr('name'),
+                    value = that.val();
+                data[name] = value;
+            });
+
+            $.ajax({
+                url: url,
+                type: type,
+                data: data,
+
+                success: function (response) {
+                    var data = jQuery.parseJSON(response);
+                    console.log(data);
+
+                    var fb_details = {};
+                    fb_details.fb_uid = data.fb_uid;
+                    fb_details.fname = data.fb_fname;
+                    fb_details.lname = data.fb_lname;
+                    fb_details.email = data.fb_email;
+                    fb_details.picture = data.fb_profile_pic;
+
+                    writeUserData(data.lname, data.fname, data.email, data.contact, data.gender, data.id_type, data.id_number, data.dob, data.store_branch, data.currency, fb_details);
+                    $('.wing-form-wrap').addClass('remove');
+                    window.location('thankyou.php');
+                }
+            });
+
+            return false;
         } else {
             e.preventDefault();
         }
