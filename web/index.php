b327a8ef9eb77eb2eb0a9d9621bfd0e6cba83125
somielwin <elwin.delacruz@searchoptmedia.com>
2016-08-30 15:32:59 +0800

    revert js

--- web/index.php
+++ web/index.php
@@ -561,5 +561,18 @@
 	</div>
 </footer>
 <script src="js/needed-scripts.js" async></script>
+<script type="text/javascript">
+	function downloadJSAtOnload() {
+		var element = document.createElement("script");
+		element.src = "js/minified-plugins.js";
+		document.body.appendChild(element);
+	}
+	if (window.addEventListener)
+		window.addEventListener("load", downloadJSAtOnload, false);
+	else if (window.attachEvent)
+		window.attachEvent("onload", downloadJSAtOnload);
+	else window.onload = downloadJSAtOnload;
+
+</script>
 </body>
 </html>
