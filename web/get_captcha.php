bd5fee2be0fdf0271faabb3016b59ea1a704c0fa
somielwin <elwin.delacruz@searchoptmedia.com>
2016-10-17 15:31:08 +0800

    test captcha url

--- web/get_captcha.php
+++ web/get_captcha.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+
 $string = '';
 
 for ($i = 0; $i < 5; $i++) {
@@ -43,8 +44,7 @@ imagefilledrectangle($image,0,0,399,99,$white);
 imagettftext ($image, 30, 0, 10, 40, $color, $dir.$font, $_SESSION['random_number']);
 
 header("Content-type: image/png");
-/**imagepng($image);**/
+imagepng($image);
 
 
-return imagepng($image);
 ?>
