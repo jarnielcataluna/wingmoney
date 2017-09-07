6a8c1a2877926c99db307e892cd85e3b53132b76
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2017-09-07 21:15:48 +0800

    remove analytics for now

--- web/pchum-ben.php
+++ web/pchum-ben.php
@@ -37,11 +37,11 @@ session_start();
 <!-- <script src='https://www.google.com/recaptcha/api.js?manual_challenge=false' defer></script>  -->
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KHPB6M2');</script>
+})(window,document,'script','dataLayer','GTM-KHPB6M2');</script> -->
 <!-- End Google Tag Manager -->
 </head>
 <body>
