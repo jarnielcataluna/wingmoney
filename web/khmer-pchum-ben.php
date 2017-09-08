35e5c1bffcab655277cdfc5e9db583c229e2e6b8
Elwin Dela Cruz <somi-elwin@somi-elwins-MacBook-Pro.local>
2017-09-08 15:39:21 +0800

    add promo lp

---
+++ web/khmer-pchum-ben.php
@@ -0,0 +1,154 @@
+<?php
+
+session_start();
+?>
+<!doctype html>
+<html class="no-js" lang="km">
+<head>
+<meta charset="utf-8">
+<meta name="robots" content="noindex,follow" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Wing Money | Pchum Ben</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#b0c025">
+<meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#b0c025">
+
+<link rel="stylesheet" href="css/style.css" media="all"/>
+<link rel="stylesheet" href="css/promo-style.css" media="all"/>
+
+<!--[if lt IE 9]> <script src="js/css3-mediaqueries.js"></script> <![endif]-->
+<!-- <script src='https://www.google.com/recaptcha/api.js?manual_challenge=false' defer></script>  -->
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KHPB6M2');</script>
+<!-- End Google Tag Manager -->
+</head>
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHPB6M2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<section id="main-container">
+    <section id="main-wrapper" class="promo-layout khmer-lang">
+
+        <header>
+            <div class="home-header">
+				<div class="container">
+					<div class="w-logo fl"><img src="images/page_template/logo.png" alt=""></div>
+					<!-- <a href="form.php" class="apply-now fr">Watch Video</a> -->
+					<div class="wpml-languages enabled">
+						<a class="active tooltip" data-tooltip="No translations available for this page">
+							<img src="images/km.png" alt="" width="18" height="12">
+							<i class="fa fa-angle-down" aria-hidden="true"></i>
+						</a>
+						<ul class="wpml-lang-dropdown">
+							<li><a href="pchum-ben.php"><img src="images/en.png" alt="" width="18" height="12"></a></li>
+						</ul>
+					</div>
+					<div class="clr"></div>
+				</div>
+			</div>
+        </header>
+
+        <section class="main-wrapper">
+            <div class="pl-banner-wrap">
+                <img class="pl-desktop animated hiding" data-animation="fadeIn" data-delay="300" src="images/promo/khmer-banner.jpg" alt="">
+                <img class="pl-mobile animated hiding" data-animation="fadeIn" data-delay="300" src="images/promo/mobile-khmer.jpg" alt="">
+            </div>
+            <div class="container">
+                
+                <div class="pl-row">
+                    <div class="pl-content">
+                        <article>
+                            <h2>ការផ្តល់ជូនថ្ងៃបុណ្យភ្ជុំបិណ្ឌ</h2>
+                            <p>ធនាគារឯកទេសវីងដែលឈានមុខគេផ្នែកសេវាកម្មធនាគារតាមទូរស័ព្ទដៃនៅកម្ពុជាក្នុងឱកាសបុណ្យភ្ជុំបិណ្ឌឆ្នាំនេះ នឹងផ្តល់ជូនសំបុត្រឡានក្រុងសម្រាប់ធ្វើដំណើរទៅកាន់ខេត្តណាមួយទាំង២៥ ពីក្រុមហ៊ុនភ្នំពេញសូរិយា និងទឹកសុទ្ធអរុណាចំនួនមួយកេសដោយឥតគិតថ្លៃ ទៅ​ដល់អតិថិជន​ដែល​មានគណនីវីង នៅរាល់ការធ្វើប្រតិបត្តិការផ្សេងៗ ចាប់ពី៥ដុល្លារឡើងទៅ។</p>
+                            <p>លក្ខខណ្ឌដើម្បីទទួលបាននូវការផ្តល់ជូនដ៏ពិសេសនេះ អតិថិជនដែលមានគណនីវីងគ្រាន់តែធ្វើតាមការណែនាំតាមវិធីបីងាយៗដូចតទៅ៖</p>
+                            <ul>
+                                <li class="lower-latin-wrap"><span class="lower-latin-text">១)</span> <span>ធ្វើប្រតិបត្តិការផ្សេងៗជាមួយគណនីវីងចាប់ពី៥ដុល្លារឡើងទៅ ចាប់ពីថ្ងៃទី១ កញ្ញា ដល់ ថ្ងៃទី១៨ ខែកញ្ញា ឆ្នាំ២០១៧</span></li>
+                                <li class="lower-latin-wrap"><span class="lower-latin-text">២)</span> <span>ធ្វើដំណើរទៅកាន់ស្តង់វីងដែលមានទីតំាងនៅកន្លែងលក់សំបុត្រឡានក្រុងសូរិយាផ្សារធំថ្មី និងបង្ហាញកំណត់ត្រាទាក់ទងនឹងការធ្វើប្រតិបត្តិការផ្សេងៗជាមួយគណនីវីង</span></li>
+                                <li class="lower-latin-wrap"><span class="lower-latin-text">៣)</span> <span>ទទួលយកសំបុត្រ និងកក់សំបុត្រ ២ ទៅ ៤ ថ្ងៃមុនការធ្វើដំណើររបស់អ្នក ដោយហៅទូរស័ព្ទ ឬធ្វើដំណើរទៅកាន់កន្លែងលក់សំបុត្រឡានក្រុងដោយផ្ទាល់</span></li>
+                            </ul>
+                           
+                        </article>
+                    </div>
+                    <div class="box-wrap">
+                        <h4>លក្ខខណ្ឌចូលរួមក្នុងការផ្តល់ជូនដ៏ពិសេសនេះ៖</h4>
+                        <ul>
+                            <li>ផ្ញើប្រាក់ពីគណនីវីងទៅវីង</li>
+                            <li>ផ្ញើប្រាក់ពីគណនីវីងទៅវីងវេរលុយ</li>
+                            <li>ការបញ្ចូលលុយទូរស័ព្ទ</li>
+                            <li>ការទូទាត់វិក្កយបត្រតាមអ៊ិនធឺណែត</li>
+                            <li>ការទូទាត់វិក្កយបត្រ</li>
+                            <li>វវីងគិតលុយ</li>
+                            <li>ការទូទាត់វិក្កយបត្រទឹកភ្លើងលើសពី២៥ដុលា្លរ</li>
+                        </ul>
+                    </div>
+                    <div class="clr"></div>
+                     <p>ចំណាំ៖ អតិថិជនអាចទៅយកសំបុត្រចាប់ពីថ្ងៃទី ៩ ដល់ ថ្ងៃទី ១៨ ខែកញ្ញាឆ្នាំ ២០១៧ ពីម៉ោង ៨​ : ០០ នាទីព្រឹក ដល់ ម៉ោង ៥ : ០០ នាទីល្ងាច។</p>
+                    <p>សម្រាប់ការធ្វើប្រតិបត្តិការតាមអនឡាញវិញ អតិថិជនដែលមានគណនីវីងក៏នឹងទទួលបានការបញ្ចុះតម្លៃពិសេសចំនួន១០% ពីក្រុមហ៊ុនឡានក្រុងដូចជា ឡារីតា វីរៈប៊ុនថាំ ភីអេសឌី ម៉ូនឡាយឃីស ប៊ុកមីបាស និងការបញ្ចុះតម្លៃចំនួន១ដុល្លារពីក្រុមហ៊ុនមេគង្គអិចប្រេស៕</p>
+                </div>
+                
+            </div>
+        </section>
+    </section>
+</section>
+
+<footer class="promo-footer khmer-lang">
+
+    <div class="container">
+        <div class="wing-app-detail">
+
+            <h3>កម្មវិធីស្មាតហ្វូនវីង</h3>
+            <p>ដំឡើងកម្មវិធីស្មាតហ្វូនវីងលើទូរស័ព្ទរបស់អ្នក ប្រើប្រាស់គណនីវីងរបស់អ្នក និងសេវាកម្មវីងទាំងអស់ភ្លាមៗ គ្រប់ពេលវេលា និងគ្រប់ទីកន្លែង</p>
+            <a href="https://itunes.apple.com/app/apple-store/id1113286385?pt=118405355&ct=pchumben_promotion&mt=8" target="_blank" class="dl-app dl-app-ios"><img src="images/promo/final-ios-store.png"></a>
+            <a href="https://play.google.com/store/apps/details?id=com.wingmoney.wingpay&referrer=utm_source%3Dmicrosite%26utm_medium%3Dcta%26utm_campaign%3Dpchumben_promotion" target="_blank" class="dl-app dl-app-android"><img src="images/promo/final-google-store.png"></a>
+        </div>
+        <div class="wing-app-img"><img src="images/promo/wing-mobile-img.png" alt=""></div>
+    </div>
+</footer>
+<script src="js/needed-scripts.js" ></script>
+<script src="js/pace.min.js"></script>
+<script src="js/TweenMax.min.js"></script>
+<script src="js/ScrollToPlugin.min.js"></script>
+<script src="js/jquery.appear.js" ></script>
+<script src="js/devices.js" ></script>
+<script src="js/custom.js" ></script>
+<!-- <script type="text/javascript">
+    function downloadJSAtOnload() {
+        var element = document.createElement("script");
+        element.src = "js/minified-plugins.js";
+        document.body.appendChild(element);
+    }
+    if (window.addEventListener)
+        window.addEventListener("load", downloadJSAtOnload, false);
+    else if (window.attachEvent)
+        window.attachEvent("onload", downloadJSAtOnload);
+    else window.onload = downloadJSAtOnload;
+</script> -->
+</body>
+</html>
+
+
---
+++ web/pchum-ben.php
@@ -0,0 +1,154 @@
+<?php
+
+session_start();
+?>
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+<meta charset="utf-8">
+<meta name="robots" content="noindex,follow" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Wing Money | Pchum Ben</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#b0c025">
+<meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#b0c025">
+
+<link rel="stylesheet" href="css/style.css" media="all"/>
+<link rel="stylesheet" href="css/promo-style.css" media="all"/>
+
+<!--[if lt IE 9]> <script src="js/css3-mediaqueries.js"></script> <![endif]-->
+<!-- <script src='https://www.google.com/recaptcha/api.js?manual_challenge=false' defer></script>  -->
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KHPB6M2');</script>
+<!-- End Google Tag Manager -->
+</head>
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHPB6M2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<section id="main-container">
+    <section id="main-wrapper" class="promo-layout">
+
+        <header>
+            <div class="home-header">
+				<div class="container">
+					<div class="w-logo fl"><img src="images/page_template/logo.png" alt=""></div>
+					<!-- <a href="form.php" class="apply-now fr">Watch Video</a> -->
+					<div class="wpml-languages enabled">
+						<a class="active tooltip" data-tooltip="No translations available for this page">
+							<img src="images/en.png" alt="" width="18" height="12">
+							<i class="fa fa-angle-down" aria-hidden="true"></i>
+						</a>
+						<ul class="wpml-lang-dropdown">
+							<li><a href="khmer-pchum-ben.php"><img src="images/km.png" alt="" width="18" height="12"></a></li>
+						</ul>
+					</div>
+					<div class="clr"></div>
+				</div>
+			</div>
+        </header>
+
+        <section class="main-wrapper">
+            <div class="pl-banner-wrap">
+                <img class="animated hiding" data-animation="fadeIn" data-delay="300" src="images/promo/english-banner.jpg" alt="">
+                <!-- <img class="pl-mobile animated hiding" data-animation="fadeIn" data-delay="300" src="images/promo/mobile-english.jpg" alt=""> -->
+            </div>
+            <div class="container">
+                
+                <div class="pl-row">
+                    <div class="pl-content">
+                        <article>
+                            <h2>Promotion for Pchum Ben Festival</h2>
+                            <p>WING, the leading mobile banking company in Cambodia, will be offering FREE bus tickets from Phnom Penh Sorya to any 25 provinces and a free box of Aruna pure water to WING Account customers who make any transactions from $5 up for Pchum Ben this year.</p>
+                            <p>In order to receive this special offer, the customers who have WING Account can simply follow 3 easy instructions below:</p>
+                            <ul>
+                                <li class="lower-latin-wrap"><span class="lower-latin-text">1)</span> <span>Perform any transactions with WING accounts from $5 up starting from 1 September until 18 September 2017</span></li>
+                                <li class="lower-latin-wrap"><span class="lower-latin-text">2)</span> <span>Visit WING booth located at Central Market Sorya Ticket Counter and show your history of any transactions with WING account.</span></li>
+                                <li class="lower-latin-wrap"><span class="lower-latin-text">3)</span> <span>Get the ticket and book the bus tickets at least 2 or 4 days before your trip by calling or visiting bus ticket counters directly.</span></li>
+                            </ul>
+                            
+                        </article>
+                    </div>
+                    <div class="box-wrap">
+                        <h4>Terms and conditions to get this special offer:</h4>
+                        <ul>
+                            <li>Send money from Wing Account to Wing Account</li>
+                            <li>Send money from Wing Account to Wing Wei Luy</li>
+                            <li>Phone Top Up</li>
+                            <li>Online Payment</li>
+                            <li>Bills Payment</li>
+                            <li>Pay with Wing</li>
+                            <li>Utilities payment more than $25</li>
+                        </ul>
+                    </div>
+                    <div class="clr"></div>
+                    <p><em>Note: Customers can redeem the ticket from 8:00 am until 5:00pm, starting from 09 to 18 September 2017</em></p>
+                    <p>For any online payment, customers who have Wing Account will get 10% discount from Bus Companies such as Larryta, Vireak Buntham, PSD, Moonlight Kiss, BookMeBus and get 1 US dollar from Mekong Express.</p>
+                </div>
+                
+            </div>
+        </section>
+    </section>
+</section>
+
+<footer class="promo-footer">
+
+    <div class="container">
+        <div class="wing-app-detail">
+            <h3>The WING MOBILE APP</h3>
+            <p>Install Wing App on your phone, use your Wing Account and enjoy all Wing services instantly anytime, anywhere!</p>
+            <p></p>
+            <a href="https://itunes.apple.com/app/apple-store/id1113286385?pt=118405355&ct=pchumben_promotion&mt=8" target="_blank" class="dl-app dl-app-ios"><img src="images/promo/final-ios-store.png"></a>
+            <a href="https://play.google.com/store/apps/details?id=com.wingmoney.wingpay&referrer=utm_source%3Dmicrosite%26utm_medium%3Dcta%26utm_campaign%3Dpchumben_promotion" target="_blank" class="dl-app dl-app-android"><img src="images/promo/final-google-store.png"></a>
+        </div>
+        <div class="wing-app-img"><img src="images/promo/wing-mobile-img.png" alt=""></div>
+    </div>
+</footer>
+<script src="js/needed-scripts.js" ></script>
+<script src="js/pace.min.js"></script>
+<script src="js/TweenMax.min.js"></script>
+<script src="js/ScrollToPlugin.min.js"></script>
+<script src="js/jquery.appear.js" ></script>
+<script src="js/devices.js" ></script>
+<script src="js/custom.js" ></script>
+<!-- <script type="text/javascript">
+    function downloadJSAtOnload() {
+        var element = document.createElement("script");
+        element.src = "js/minified-plugins.js";
+        document.body.appendChild(element);
+    }
+    if (window.addEventListener)
+        window.addEventListener("load", downloadJSAtOnload, false);
+    else if (window.attachEvent)
+        window.attachEvent("onload", downloadJSAtOnload);
+    else window.onload = downloadJSAtOnload;
+</script> -->
+</body>
+</html>
+
+
