8246015d01586adf24eaf95d3a0886616ae4229c
somidex <dexter.loor@propelrr.com>
2017-09-09 16:27:35 +0800

    Update khmer-pchum-ben.php

--- web/khmer-pchum-ben.php
+++ web/khmer-pchum-ben.php
@@ -169,7 +169,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         // window.location.href = _href;
 
         sendGaEvent($(this), function() {
-            window.location.href = _href;
+            //window.location.href = _href;
         });
     });
 
