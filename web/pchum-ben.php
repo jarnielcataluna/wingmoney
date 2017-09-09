f3e3f6ea79e21cecf19d6344e000df8f97e24fe9
somidex <dexter.loor@propelrr.com>
2017-09-10 00:16:50 +0800

    Update pchum-ben.php

--- web/pchum-ben.php
+++ web/pchum-ben.php
@@ -200,7 +200,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             'eventCategory': _eventCategory,
             'eventAction': _eventAction,
             'eventLabel': _eventLabel,
-	    'elementId': element.attr('id');
+	    'elementId': element.attr('id'),
             'eventCallback': function() {
                 if (typeof callback == 'function') {
                     callback();
