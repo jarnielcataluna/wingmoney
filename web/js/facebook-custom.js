350fce3c248b304fc9592410e042157cbdf5b637
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2018-11-20 18:44:44 +0800

    Update facebook-custom.js

--- web/js/facebook-custom.js
+++ web/js/facebook-custom.js
@@ -25,7 +25,7 @@ $('#useFacebook').click(function(e) {
             FB.login(function(){
                 useFacebook();
                 $('#birthday-label').css({'opacity' : 0});
-            }, {scope: 'publish_actions, email, public_profile, user_birthday, user_location, user_likes, user_education_history, pages_messaging_phone_number'});
+            }, {scope: 'email, public_profile, user_birthday, user_location, user_likes, user_education_history, pages_messaging_phone_number'});
         }
     });
 });
