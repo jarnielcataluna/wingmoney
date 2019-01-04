f26183483da4fe3f4687b57f8b1f09932f552eea
Elwin Dela Cruz <propelrr-elwin@PropelrrElwin.local>
2019-01-04 11:34:32 +0800

    change khmer fonts

--- web/khmer-thankyou.php
+++ web/khmer-thankyou.php
@@ -38,7 +38,7 @@
 <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
 <meta name="theme-color" content="#b0c025">
 
-<link rel="stylesheet" href="css/style3.css" media="all"/>
+<link rel="stylesheet" href="css/style.css" media="all"/>
 
 <!--[if lt IE 9]> <script src="js/css3-mediaqueries.js"></script> <![endif]-->
 <!-- Google Tag Manager -->
